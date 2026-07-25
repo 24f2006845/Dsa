e93d736de813996064d02890e6e85dbe011881d4
Haider <Haiderkg78@gmail.com>
2026-07-25 18:41:02 +0530

    Revise MoveZero_withoutSpace

--- 03_Arrays/Hard/MoveZero_withoutSpace.py
+++ 03_Arrays/Hard/MoveZero_withoutSpace.py
@@ -1,11 +1,12 @@
+
 class Solution:
 
     def moveZeros(self, arr):
         # write your code here
-        j = 0
+        j=0
         n = len(arr)
         for i in range(n):
             if arr[i]!=0:
-                arr[j],arr[i]=arr[i],arr[j]
+                arr[i],arr[j] = arr[j],arr[i]
                 j+=1
         return arr
